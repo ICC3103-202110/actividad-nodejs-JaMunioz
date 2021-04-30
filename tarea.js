fd2cb258785c2ec1a235f0ea87146b2284525dbd
JaMunioz <jamunoz6@miuandes.cl>
2021-04-30 18:16:36 -0400

    third commit

--- tarea.js
+++ tarea.js
@@ -5,8 +5,4 @@ function sum(a, b){
 console.log(sum(2,5)); //uso de funciones
 console.log("Hola"); //print cotidiano
 x = 1;
-//printf("%d is an integer\n",x);
 console.log(x);
-//scanf("%d",x);
-//
-//
