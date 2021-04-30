56b10849c1af5b45145c3fd60f53de5e32c45074
JaMunioz <jamunoz6@miuandes.cl>
2021-04-30 19:50:49 -0400

    third commit

--- tarea.js
+++ tarea.js
@@ -1,8 +1,22 @@
-function sum(a, b){
-    return a + b;
+function F(a){
+    if (a == 0){
+        return 0;
+    }
+    if (a == 1){
+        return 1;
+    }
+    if (a >= 1){
+        return (F(a-1) + F(a-2));
+    }
 }
 
-console.log(sum(2,5)); //uso de funciones
-console.log("Hola"); //print cotidiano
-x = 1;
-console.log(x);
+lista = [100,2];
+cond = 2;
+if (cond == 1){
+    console.log("Holaa"); //print cotidiano
+}
+if (cond == 2){
+    console.log(F(6)); //uso de funciones
+}
+//lista.length --> Entrega el largo de una lista.
+
