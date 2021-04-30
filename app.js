4e58439741944db1538dd659a42e837056b98759
JaMunioz <jamunoz6@miuandes.cl>
2021-04-30 18:05:36 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function sum(a, b){
+    return a + b;
+}
+
+console.log(sum(2,5)); //uso de funciones
+console.log("Hola"); //print cotidiano
+x = 1;
+//printf("%d is an integer\n",x);
+console.log(x);
+//scanf("%d",x);
