6747436f9f6765b32bdb847ef7c49b432be1e7ca
JaMunioz <jamunoz6@miuandes.cl>
2021-04-30 21:27:43 -0400

    fifth commit

--- tarea.js
+++ tarea.js
@@ -2,42 +2,36 @@ function F(a){
     if (a == 0){
         return 0;
     }
-    if (a == 1){
+    else if (a == 1){
         return 1;
     }
-    if (a >= 1){
+    else if (a >= 1){
         return (F(a-1) + F(a-2));
     }
-    if (a < 1){
+    else if (a <= -1){
         return -1; 
     } 
     return -2;
 }
 
-const readline = require('readline').createInterface({ //OPCION 1:
+
+const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
   });
    
-  readline.question('Ingrese el numero que quiere evaluar en la secuencia de figonacchi: ', n => {
-    if (n == -1){
-        console.log(`No se puede hacer con numeros menores a 0.`);
+  readline.question('\nIngrese el numero que quiere evaluar en la secuencia de'
+  +' figonacchi: ', n => {
+    if (F(n) == -1){
+        console.log(`\nNo se puede hacer con numeros menores a 0.`);
     }
     else{
-        console.log(`El resultado es: ${F(n)}`);
+        if (F(n) == -2){
+            console.log(`\nDebe ingresar numeros.`);
+        } 
+        else{
+            console.log(`\nEl resultado es: ${F(n)}`);
+        }
     }
     readline.close();
   });
-
-  /*
-  var prompt = require('prompt-sync')();//OPCION 2:
- 
-  var numero = prompt('Ingrese el numero que quiere evaluar en la secuencia de figonacchi: ');
-  console.log(`El resultado es: ${F(numero)}`);
-
-
-
-//console.log(F({name})); //uso de funciones
-
-//lista.length --> Entrega el largo de una lista.
-*/
